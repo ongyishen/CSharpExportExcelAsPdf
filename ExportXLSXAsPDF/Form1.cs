476f2a6f77b5041d2be637d6993f6e622f549678
ONG YI SHEN <ongyishen@yahoo.com>
2021-07-27 17:31:35 +0800

    Yeah

---
+++ ExportXLSXAsPDF/Form1.cs
@@ -0,0 +1,122 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ExportXLSXAsPDF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string ExportPath
+        {
+            get
+            {
+                var path = Path.Combine(Application.StartupPath, "Export");
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+                return path;
+            }
+        }
+
+        public string ExcelFileName
+        {
+            get
+            {
+                return "Sample.xlsx";
+            }
+        }
+        public string PdfFileName
+        {
+            get
+            {
+                return "Sample.pdf";
+            }
+        }
+
+        public DataTable getData()
+        {
+            //Creating DataTable  
+            DataTable dt = new DataTable();
+            //Setiing Table Name  
+            dt.TableName = "EmployeeData";
+            //Add Columns  
+            dt.Columns.Add("ID", typeof(int));
+            dt.Columns.Add("Name", typeof(string));
+            dt.Columns.Add("City", typeof(string));
+            //Add Rows in DataTable  
+            dt.Rows.Add(1, "Anoop Kumar Sharma", "Delhi");
+            dt.Rows.Add(2, "Andrew", "U.P.");
+            dt.AcceptChanges();
+            return dt;
+        }
+
+        public void WriteDataToExcel()
+        {
+            DataTable dt = getData();
+
+            string fileName = Path.Combine(ExportPath, ExcelFileName);
+            using (XLWorkbook wb = new XLWorkbook())
+            {
+                //Add DataTable in worksheet  
+                wb.Worksheets.Add(dt);
+                wb.SaveAs(fileName);
+            }
+        }
+
+        public void ExportAsPdf()
+        {
+            string fileNameExcel = Path.Combine(ExportPath, ExcelFileName);
+            string fileNamePdf = Path.Combine(ExportPath, PdfFileName);
+            if (File.Exists(fileNameExcel))
+            {
+                //PDF
+                using (Spire.Xls.Workbook workbook = new Spire.Xls.Workbook())
+                {
+                    workbook.LoadFromFile(fileNameExcel);
+
+
+                    workbook.SaveToFile(fileNamePdf, Spire.Xls.FileFormat.PDF);
+                }
+
+
+                if (File.Exists(fileNamePdf))
+                {
+                    Process.Start(fileNamePdf);
+                }
+            }
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            btnRun.Enabled = false;
+            Application.DoEvents();
+
+            try
+            {
+                //Export Excel
+                WriteDataToExcel();
+
+                //Generate Pdf
+                ExportAsPdf();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+            btnRun.Enabled = true;
+            Application.DoEvents();
+
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # CSharpExportExcelAsPdf
-C# export excel as pdf without office installed
+C# export excel as pdf without office installed (.net framework 4.5.2)
+
+## Donation
+
+If this project helped you reduce time to develop, please consider buying me a cup of coffee :)
+
+<a href="https://www.buymeacoffee.com/ongyishen" 
+target="_blank">
+<img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" 
+alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+
+
+
+##### Nuget Package
+
+- ClosedXML
+- FreeSpire.XLS
+
+
+
+Screenshots:
+<img src="https://github.com/ongyishen/CSharpExportExcelAsPdf/blob/main/Sample.gif?raw=true" />
